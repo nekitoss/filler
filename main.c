f8ac12e21b6d22099da745856ad404cf12b1aea3
Nick <nekit_p@ukr.net>
2017-07-22 18:53:01 +0300

    error and debug message functions, some reading & validation of header

--- main.c
+++ main.c
@@ -15,12 +15,6 @@ typedef struct	s_filler
 	char	c;
 }				t_filler;
 
-void			struct_delete(t_filler	**ls)
-{
-	free(*ls);
-	*ls = NULL;
-}
-
 void	debug_msg(char *str)
 {
 #ifdef DEBUG
@@ -43,6 +37,71 @@ void	debug_msg_nonl(char *str)
 #endif
 }
 
+void			error_msg(char *str)
+{
+	ft_putstr_fd("\033[01;31m", 2);
+	ft_putstr_fd("EROR: ", 2);
+	ft_putstr_fd(str, 2);
+	ft_putendl_fd("\033[0m", 2);
+	while(1);
+}
+
+void			struct_delete(t_filler	**ls)
+{
+	free(*ls);
+	*ls = NULL;
+}
+
+// void			renew_array
+
+void			read_header(t_filler *ls)
+{
+	char		*line;
+
+	if(get_next_line(0, &line) && ft_strnequ(line, "$$$ exec p", 10))
+	{
+		if (line[10] == '2')
+			ls->c = 'X';
+		else
+			ls->c = 'O';
+		ft_strdel(&line);
+	}
+	else
+		error_msg("while reading first line!");
+	debug_msg_nonl("player_letter="); debug_msg(&(ls->c));
+	if(get_next_line(0, &line) && ft_strnequ(line, "Plateau ", 8))
+	{
+		line = ft_strsub_d(&line, ft_strchr(line, ' ') - line + 1, ft_strlen(line));
+		ls->map_h = ft_atoi(line);
+		line = ft_strsub_d(&line, ft_strchr(line, ' ') - line, ft_strlen(line));
+		ls->map_w = ft_atoi(line);
+		ft_strdel(&line);
+	}
+	else
+		error_msg("while reading 2nd line!");
+	debug_msg_nonl("map_width="); debug_msg(ft_itoa_u(ls->map_w));
+	debug_msg_nonl("map_height="); debug_msg(ft_itoa_u(ls->map_h));
+}
+
+void			read_piece(t_filler *ls)
+{
+	char		*line;
+
+	if(get_next_line(0, &line) && ft_strnequ(line, "Piece ", 6))
+	{
+		line = ft_strsub_d(&line, ft_strchr(line, ' ') - line + 1, ft_strlen(line));
+		ls->fig_h = ft_atoi(line);
+		line = ft_strsub_d(&line, ft_strchr(line, ' ') - line, ft_strlen(line));
+		ls->fig_w = ft_atoi(line);
+		ft_strdel(&line);
+	}
+	else
+		error_msg("while reading piece info line!");
+	debug_msg_nonl("fig_width="); debug_msg(ft_itoa_u(ls->fig_w));
+	debug_msg_nonl("fig_height="); debug_msg(ft_itoa_u(ls->fig_h));
+	renew_array(ls);
+}
+
 int				main(void)
 {
 	char		*line;
@@ -51,36 +110,17 @@ int				main(void)
 
 	i = 0;
 	ls = (t_filler *)ft_memalloc(sizeof(t_filler));
-	if(get_next_line(0, &line))
-	{
-		// debug_msg((line + 10));
-		if (line[10] == '2')
-			ls->c = 'X';
-		else
-			ls->c = 'O';
-		ft_strdel(&line);
-	}
-	else
-		debug_msg("eror in first gnl read");
-	debug_msg_nonl("player_letter="); debug_msg(&(ls->c));
-	if(get_next_line(0, &line))
-	{
-		line = ft_strchr(line, ' ') + 1;
-		ls->map_h = ft_atoi(line);
-		line = ft_strchr(line, ' ') + 1;
-		ls->map_w = ft_atoi(line);
-		// ft_strdel(&line);
-	}
-	else
-		debug_msg("eror in 2nd gnl read");
-	debug_msg_nonl("map_width="); debug_msg(ft_itoa_u(ls->map_w));
-	debug_msg_nonl("map_height="); debug_msg(ft_itoa_u(ls->map_h));
-	while (i < 28 && get_next_line(0, &line))
+	read_header(ls);
+	get_next_line(0, &line);
+
+	while (i < ls->map_h && get_next_line(0, &line))
 	{
 		debug_msg(line);
 		ft_strdel(&line);
 		i++;
 	}
+	read_piece(ls);
 	struct_delete(&ls);
+	error_msg("exiting");
 	return (0);
 }
